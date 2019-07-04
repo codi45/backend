70f0f9f500a2de03ca1f8ae032345d3463124a76
codi45 <codi4572@gmail.com>
2019-07-04 12:19:06 -0400

    add cors

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.get('/augurCategories', (req, res) => augur.augur.markets.getMarkets(req, res));
